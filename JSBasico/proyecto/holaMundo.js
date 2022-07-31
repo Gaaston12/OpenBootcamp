e1f3f2bdc13457d1d08065b6eb0581bd42b18e3e
Gaston Martin <gaastonmaartin12@gmail.com>
2022-07-31 17:03:18 -0300

    Exercise One for the course of Basic JS

---
+++ JSBasico/proyecto/holaMundo.js
@@ -0,0 +1,7 @@
+console.log("Hola mundo");
+
+// Github del proyecto
+console.log("https://github.com/Open-Bootcamp/JavaScript-Basico");
+
+//Ejecicicio 1 
+console.log("Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo");
